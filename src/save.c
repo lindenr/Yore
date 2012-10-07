950c9701f5beed54c6bd3a6907609fa53fbc1f8f
lindenr <lindenr@fastmail.fm>
2012-10-07 02:03:41 +0100

    fixed some bugs in saving and added starts of a loading feature.

--- src/save.c
+++ src/save.c
@@ -43,7 +43,7 @@ struct block_ptr
 	int len;
 };
 
-struct List blocks;
+struct List blocks = LIST_INIT;
 
 void store_block(void *ptr, int len)
 {
@@ -76,7 +76,11 @@ void save_item(struct Item *item)
 	SAVE_NATIVE(item->type);
 	SAVE_NATIVE(item->attr);
 	SAVE_NATIVE(item->cur_weight);
-	fwrite(item->name, strlen(item->name)+1, 1, game_save_file);
+
+	if (!item->name)
+		fwrite("\0", 1, 1, game_save_file);
+	else
+		fwrite(item->name, strlen(item->name)+1, 1, game_save_file);
 }
 
 /* This directly converts currently-used memory to little-endian; on some machines it *will*
@@ -90,7 +94,7 @@ bool save(char *filename)
 	if (pask("yn", "Save and quit?") == 'y')
 	{
 		pline("Saving...");
-		game_save_file = fopen("YORE_SAVE.dat", "w");
+		game_save_file = fopen(filename, "w");
 		fwrite("YOREv"YORE_VERSION, sizeof("YOREv"YORE_VERSION)+1, 1, game_save_file);
 
 		/* Time */
@@ -158,8 +162,8 @@ bool save(char *filename)
 				case THING_DGN:
 				{
 					/* Where is it in the array? */
-					int type = (int)th->thing;
-					type -= (int)map_items;
+					uint32_t type = (uint32_t)th->thing;
+					type -= (uint32_t)map_items;
 					type /= sizeof(struct map_item_struct);
 					SAVE_NATIVE(type);
 				}
@@ -177,9 +181,125 @@ bool save(char *filename)
 	return true;
 }
 
+#define LOAD_NATIVE(n) fread(&n, sizeof(n), 1, game_save_file); memconv(&n, sizeof(n))
+
+void *alloc_block(void *ptr)
+{
+}
+
+void load_item(struct Item **out_item)
+{
+	void *ptr;
+	LOAD_NATIVE(ptr);
+	if (!ptr)
+	{
+		*out_item = NULL;
+		return;
+	}
+	struct Item *item = malloc(sizeof(*item));
+	item->type = alloc_block(ptr);
+	LOAD_NATIVE(item->attr);
+	LOAD_NATIVE(item->cur_weight);
+	char temp[100];
+	fgets(temp, 100, game_save_file);
+	int len = strlen(temp);
+	if (len < 2) item->name = NULL;
+	else
+	{
+		item->name = malloc(len+1);
+		strcpy(item->name, temp);
+	}
+
+	*out_item = item;
+}
+
 void restore(char *filename)
 {
-	// TODO restore the game
+	int i;
+	U.playing = PLAYER_ERROR; /* for premature returning */
+	game_save_file = fopen(filename, "r");
+	char *ftest = malloc(strlen("YOREv"YORE_VERSION))+1;
+
+	fread(ftest, strlen("YOREv"YORE_VERSION), 1, game_save_file);
+	if (strcmp(ftest, "YOREv"YORE_VERSION)) return;
+
+	LOAD_NATIVE(Time);
+
+	LOAD_NATIVE(U.hunger);
+	LOAD_NATIVE(U.role);
+	LOAD_NATIVE(U.playing);
+	for (i = 0; i < 6; ++ i)
+		LOAD_NATIVE(U.attr[i]);
+	LOAD_NATIVE(U.luck);
+	LOAD_NATIVE(U.m_glflags);
+	
+	do
+	{
+		struct Thing *th = malloc(sizeof(*th));
+		LOAD_NATIVE(th->type);
+		if (th->type == 0x31313131)
+		{
+			free(th);
+			break;
+		}
+		LOAD_NATIVE(th->yloc);
+		LOAD_NATIVE(th->xloc);
+		
+		switch(th->type)
+		{
+			case THING_ITEM:
+			{
+				load_item(&th->thing);
+				break;
+			}
+			case THING_MONS:
+			{
+				struct Monster *mn = malloc(sizeof(*mn));
+
+				LOAD_NATIVE(mn->type);
+				LOAD_NATIVE(mn->level);
+				LOAD_NATIVE(mn->exp);
+				LOAD_NATIVE(mn->HP);
+				LOAD_NATIVE(mn->HP_max);
+				LOAD_NATIVE(mn->cur_speed);
+				fgets(mn->name, 20, game_save_file);
+
+				for (i = 0; i < MAX_ITEMS_IN_PACK; ++ i)
+					load_item(&(mn->pack.items[i]));
+
+				load_item(&mn->wearing.head);
+				load_item(&mn->wearing.torso);
+				load_item(&mn->wearing.legs);
+				load_item(&mn->wearing.feet);
+				load_item(&mn->wearing.hands);
+				load_item(&mn->wearing.arms);
+				load_item(&mn->wearing.rfin);
+				load_item(&mn->wearing.lfin);
+				load_item(&mn->wearing.head);
+				load_item(&mn->wearing.head);
+				LOAD_NATIVE(mn->wearing.two_weaponing);
+
+				LOAD_NATIVE(mn->status);
+				load_item(&mn->eating);
+
+				th->thing = mn;
+				break;
+			}
+			case THING_DGN:
+			{
+				uint32_t num;
+				LOAD_NATIVE(num);
+				th->thing = &map_items[num];
+				break;
+			}
+			default:
+			{
+			}
+		}
+		push_back(&all_things, th);
+	}
+	while (1);
+
 	U.playing = PLAYER_PLAYING;	/* success */
 }
 
